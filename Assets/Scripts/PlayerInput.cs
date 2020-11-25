9e46748db2f7a1594deeb4f9234e95cee9395c45
Joe Van Deusen <daeleth@gmail.com>
2020-11-25 10:23:59 -0500

    Loading in some scripts that might be useful to us.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+    Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        player.SetDirectionalInput(directionalInput);
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.OnJumpInputDown();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            player.OnJumpInputUp();
+        }
+
+        // Shooting/grapple
+        if (Input.GetMouseButtonDown(0))
+        {
+            player.OnMouseButtonDown();
+        }
+    }
+}
