c542e2e28039df6796205b7a736fc64163218975
Verywiseguy7 <Robert_Martin1@student.uml.edu>
2020-12-12 13:07:24 -0500

    Back and Forth using physics
    Testing a method of moving a platform back and forth using forces, which maintains physics behaviors for things hanging from it.
    
    Still now player hanging from it yet, just a test object.

--- Assets/Scripts/blockBackAndForth_automove.cs
+++ Assets/Scripts/blockBackAndForth_automove.cs
@@ -11,6 +11,8 @@ public class blockBackAndForth_automove : MonoBehaviour
 
     private float moveDir;
     private Vector3 moveVector;
+    Rigidbody2D myRigidBody;
+    Vector3 initialPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +22,35 @@ public class blockBackAndForth_automove : MonoBehaviour
         //moveVector = transform.up;
 
         moveVector = Quaternion.AngleAxis(initialMoveDirection, Vector3.forward) * Vector3.right;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+
+        myRigidBody.AddForce(moveVector * myRigidBody.mass * moveSpeed * moveDir, ForceMode2D.Impulse);
 
         InvokeRepeating("flipVelocity", reverseTime, reverseTime);
+        InvokeRepeating("resetPosition", 2 * reverseTime, 2 * reverseTime);
         
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(moveVector * moveDir * moveSpeed * Time.deltaTime);
+        //Works, but bypasses physics.  Comment out forces and uncomment this to test
+        //transform.Translate(moveVector * moveDir * moveSpeed * Time.deltaTime);
     }
 
     void flipVelocity()
     {
         moveDir = -1 * moveDir;
+        myRigidBody.AddForce(2 * moveVector * myRigidBody.mass * moveSpeed * moveDir, ForceMode2D.Impulse);
+
+    }
+
+    //Although the mass is large, technically it can be pulled away if you wait long enough
+    //This function is a bandaid to prevent this, but putting it back where it started every time
+    //the cycle repeats.  This technically cheats the physics but is neglegible.
+    void resetPosition() 
+    {
+        transform.position = initialPosition;
     }
 }
