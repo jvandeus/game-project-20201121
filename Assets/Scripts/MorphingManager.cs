e6fe340c387db990df5cebcd3cb79ab2f2f34fdf
Verywiseguy7 <Robert_Martin1@student.uml.edu>
2020-12-30 16:10:19 -0500

    Multiple Forms to choose from, Toggling, and UI shows current selection
    Adding new forms right now is as simple as adding another FormHolder_ child to the player, and choosing the sprite you want.
    
    Morphing Manager gets a list of all of those children, and lets you use chosen form up and down buttons to toggle through them (Q and E for right now).
    
    When you hit Q or E, you move up or down the list of available forms (skipping the base, which is always what you revert back to when you release left shift).
    
    I added a UI overlay, which right now only shows which form you'll turn into when you press shift.  If you're already morphed, you can't morph into that form until you revert to base first.  Hitting Q or E updates this UI icon, as well.
    
    All these inputs are handled by the MorphingManager in Update.  We may want to move it all to CharacterController so that it's in one place.  That wouldn't be hard, but I'm leaving it as it is for now.
    
    As of right now, it's still set up so that changing form only changes the sprite.  No other properties are changed yet.
    
    Also, There's a bug in grappling that I first noticed here, but it probably goes way back.  If you mouse click on a valid grapple location, but the max distance clamp ends up putting the grapple in an invalid location, you hang from the invalid location.  This is probably because there's no validity clamp on the check.  The reverse is also a bug.  If you click on an invalid location, but the max distance clamp lands on a valid one, you don't hang.

--- Assets/Scripts/MorphingManager.cs
+++ Assets/Scripts/MorphingManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MorphingManager : MonoBehaviour
 {
+    //Public (Editable in Inspector)
     public List<GameObject> formList = new List<GameObject>();
+    public GameObject UIFormSpriteHolder; 
+
+    //Private
     private SpriteRenderer parentSpriteRenderer;
+    private int chosenFormIndex;
+    
     
 
     // Start is called before the first frame update
@@ -22,6 +29,7 @@ public class MorphingManager : MonoBehaviour
                 formList.Add(child.gameObject);
             }
         }
+        chosenFormIndex = 1;
     }
 
     // Update is called once per frame
@@ -36,11 +44,22 @@ public class MorphingManager : MonoBehaviour
         {
             morphToBaseForm();
         }
+
+        if (Input.GetButtonDown("MoveChosenFormUp"))
+        {
+            chosenFormUp();
+        }
+
+        if (Input.GetButtonDown("MoveChosenFormDown"))
+        {
+            chosenFormDown();
+        }
     }
 
     public void morphToChosenForm()
     {
-        parentSpriteRenderer.sprite = formList[1].GetComponent<SpriteRenderer>().sprite;
+        parentSpriteRenderer.sprite = formList[chosenFormIndex].GetComponent<SpriteRenderer>().sprite;
+        
 
         //Rather than setting sprites, we may eventually just want to have all the Rigidbodies and
         //Colliders and everything else in these children, and just selectively enable/disable them
@@ -52,5 +71,35 @@ public class MorphingManager : MonoBehaviour
     public void morphToBaseForm()
     {
         parentSpriteRenderer.sprite = formList[0].GetComponent<SpriteRenderer>().sprite;
+
     }
+
+    public void chosenFormUp()
+    {
+        chosenFormIndex += 1;
+
+        //If is for wrapping around the list if we go above
+        if (chosenFormIndex >= (formList.Count)) 
+        {
+            chosenFormIndex = 1;
+        }
+
+        //Update the sprite in the UI
+        UIFormSpriteHolder.GetComponent<Image>().sprite = formList[chosenFormIndex].GetComponent<SpriteRenderer>().sprite;
+    }
+
+    public void chosenFormDown()
+    {
+        chosenFormIndex -= 1;
+
+        //If is for wrapping around the list if we go below
+        if (chosenFormIndex <= 0)
+        {
+            chosenFormIndex = (formList.Count - 1); //Need the minus one because indexing starts at zero
+        }
+
+        //Update the sprite used in the UI
+        UIFormSpriteHolder.GetComponent<Image>().sprite = formList[chosenFormIndex].GetComponent<SpriteRenderer>().sprite;
+    }
+
 }
