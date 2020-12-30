a6f7346fe9eacffef5a482dae5c9d9efeb5917f4
Verywiseguy7 <Robert_Martin1@student.uml.edu>
2020-12-30 16:55:50 -0500

    Added an "Oooompf" box.
    Created a little script that applies a vertical impulse to the player when the player hits it. I tried doing this with an effector, but I couldn't customize the applied force enough to do what I want, or make it an impulse for that matter.
    
    In the inspector, you can set the final velocity you want the oomfbox to try to give.
    
    If the player hits the box with any negative vertical velocity, the impulse applied to the player is strong enough to counteract this initial negative momentum, AND result in the desired final upward velocity.
    
    If the player initially has positive vertical momentum, I didn't want to take that away, so the impulse applied just adds to that velocity.
    
    I'd like to generalize this so that I can point these boxes in any direction, but that will probably take some finagling with vectors...maybe.  Maybe just rotating the box and converting the local up to world would give me the vector I want.

---
+++ Assets/Scripts/OoomfBoxComponent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OoomfBoxComponent : MonoBehaviour
+{
+
+    public float targetVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision Occured");
+
+        Vector2 initVel = collision.otherRigidbody.velocity;
+
+        Vector2 appliedImpulse = new Vector2(0, 0);
+
+        //For now, assuming we always want straight up.  In the future we may want to generalize to any direction
+
+        //If initial Y velocity is negative, we want the impulse to add enough force to overcome that, AND achieve the desired velocity
+        if (initVel[1] < 0)  //Y component
+        {
+            appliedImpulse[1] += collision.otherRigidbody.mass * -1 * initVel[1];
+        }
+        appliedImpulse[1] += collision.otherRigidbody.mass * targetVelocity;
+
+        collision.otherRigidbody.AddForce(appliedImpulse, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        
+        Debug.Log("trigger Occured");
+
+
+        
+        Vector2 initVel = otherCollider.attachedRigidbody.velocity;
+
+        Vector2 appliedImpulse = new Vector2(0, 0);
+
+        //For now, assuming we always want straight up.  In the future we may want to generalize to any direction
+
+        //If initial Y velocity is negative, we want the impulse to add enough force to overcome that, AND achieve the desired velocity
+        if (initVel[1] < 0)  //Y component
+        {
+            appliedImpulse[1] += otherCollider.attachedRigidbody.mass * -1 * initVel[1];
+        }
+
+        //Add some impulse to achieve the desired additional impulse
+        appliedImpulse[1] += otherCollider.attachedRigidbody.mass * targetVelocity;
+
+        otherCollider.attachedRigidbody.AddForce(appliedImpulse, ForceMode2D.Impulse);
+    }
+}
