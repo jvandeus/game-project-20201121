4fa4746dffaa37c036ef14bfc18fcf22beb4e0be
Verywiseguy7 <Robert_Martin1@student.uml.edu>
2021-02-02 13:18:57 -0500

    360 Degree Ooomfbox and test stage
    * Ooomf boxes can now be rotated.  Launch direction will be in direction arrows are pointing
    * Ooomf no initial momentum is conserved any longer (shame, I'd like to do something there, but max speeds were too great).
    * Added an area to the scene for testing Ooomf boxes.  Kind of cluttered, and not the best...but it works.
    *Hierarchy in the scene is a mess now.  Not sure what the long term solution is to that.

--- Assets/Scripts/OoomfBoxComponent.cs
+++ Assets/Scripts/OoomfBoxComponent.cs
@@ -6,11 +6,17 @@ public class OoomfBoxComponent : MonoBehaviour
 {
 
     public float targetVelocity;
+    private float launchDir;
+    private Vector2 launchVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //launchDir = transform.up.magnitude + transform.eulerAngles[2] + 90; //Z Euler Angle, +90 degrees so that it's up and not right.
+        //launchVector = new Vector2(Mathf.Cos(launchDir), Mathf.Sin(launchDir));
+
+        launchVector = transform.up;
+        //launchVector.
     }
 
     // Update is called once per frame
@@ -20,48 +26,43 @@ public class OoomfBoxComponent : MonoBehaviour
     }
 
    
-    private void OnCollisionEnter2D(Collision2D collision)
+      private void OnTriggerEnter2D(Collider2D otherCollider)
     {
-        Debug.Log("Collision Occured");
-
-        Vector2 initVel = collision.otherRigidbody.velocity;
-
-        Vector2 appliedImpulse = new Vector2(0, 0);
-
-        //For now, assuming we always want straight up.  In the future we may want to generalize to any direction
-
-        //If initial Y velocity is negative, we want the impulse to add enough force to overcome that, AND achieve the desired velocity
-        if (initVel[1] < 0)  //Y component
-        {
-            appliedImpulse[1] += collision.otherRigidbody.mass * -1 * initVel[1];
-        }
-        appliedImpulse[1] += collision.otherRigidbody.mass * targetVelocity;
-
-        collision.otherRigidbody.AddForce(appliedImpulse, ForceMode2D.Impulse);
-    }
-
-    private void OnTriggerEnter2D(Collider2D otherCollider)
-    {
-        
-        Debug.Log("trigger Occured");
-
-
         
         Vector2 initVel = otherCollider.attachedRigidbody.velocity;
-
         Vector2 appliedImpulse = new Vector2(0, 0);
 
-        //For now, assuming we always want straight up.  In the future we may want to generalize to any direction
+        //Get initial velocity component along final launch vector
+        float initVelComponent = 0f;
+        initVelComponent = Vector2.Dot(initVel, launchVector.normalized); //A dot B = |A||B|cos(theta).  For unit vector B, |B| = 1;
 
-        //If initial Y velocity is negative, we want the impulse to add enough force to overcome that, AND achieve the desired velocity
-        if (initVel[1] < 0)  //Y component
+        //Clear existing velocity
+        otherCollider.attachedRigidbody.velocity = new Vector2(0, 0);
+
+        //If there was initially momentum in the launch direction, we'll want to add to it, so add it in
+        if (initVelComponent > 0)
         {
-            appliedImpulse[1] += otherCollider.attachedRigidbody.mass * -1 * initVel[1];
+
+            //I really liked being able to maintain some momentum...but it was too unpredictable.
+            //Maybe we can maintain some X component...but not Y because we need all we can against gravity
+
+            //Imp += m*v*unitVector
+            //appliedImpulse += otherCollider.attachedRigidbody.mass * initVelComponent * launchVector.normalized;
         }
 
-        //Add some impulse to achieve the desired additional impulse
-        appliedImpulse[1] += otherCollider.attachedRigidbody.mass * targetVelocity;
+        // Add impulse to get desired final velocity
+        appliedImpulse += otherCollider.attachedRigidbody.mass * targetVelocity * launchVector;
 
+        //Apply the change in momentum as an impulse force
         otherCollider.attachedRigidbody.AddForce(appliedImpulse, ForceMode2D.Impulse);
+
+        //A better approach may just be to do special removal of downward velocity.  Right now a 
+        //straight up oomf box removes all horizonal momentum, which doesn't feel great.
+
+
+        //Alternative, never exceeds a max velocity
+
+
+
     }
 }
